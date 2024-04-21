e6f6c0ede2782d504a14709ae4df0cddb7f2fe3a
Carlos Otávio de Souza <carlosotavio_souza@apple.com>
2024-04-21 17:43:53 -0300

    first commit

---
+++ backend/index.js
@@ -0,0 +1,50 @@
+import express from "express";
+import http from "http";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+
+import mergedResolvers from "./resolvers/index.js";
+import mergedTypeDefs from "./typeDefs/index.js";
+
+import { connectDB } from "./db/connectDB.js";
+
+import passport from "passport";
+import session from "express-session";
+import connectMongo from "connect-mongodb-session";
+
+dotenv.config();
+const app = express();
+const httpServer = http.createServer(app);
+
+const MongoDBStore = connectMongo(session);
+
+const store = new MongoDBStore({
+  uri: process.env.MONGO_URI,
+});
+
+// Setting up graphQL server:
+const server = new ApolloServer({
+  typeDefs: mergedTypeDefs,
+  resolvers: mergedResolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+
+await server.start();
+
+app.use(
+  "/",
+  cors(),
+  express.json(),
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ req }),
+  })
+);
+
+// Modified server startup
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+await connectDB();
+console.log(`🚀 Server ready at http://localhost:4000/`);
